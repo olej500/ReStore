308ded148961d1f45f0f32e17aed1c0c27fb2829
olej500 <olej500@gmail.com>
2023-10-20 19:43:45 +0200

    ENd of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
